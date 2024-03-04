90f3aa54cc239b0482b31c42862054fe0bc1c581
muldersgf69 <109549032+muldersgf69@users.noreply.github.com>
2024-03-04 00:22:33 +0000

    final commit

--- src/js/component/App.jsx
+++ src/js/component/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Home from '../Home';
-import Cards from '../Cards';
+import Cards from './Cards';
+import Home from './Home';
 
 function App() {
   return (
--- src/js/component/Cards.jsx
+++ src/js/component/Cards.jsx
@@ -1,7 +1,7 @@
 import React from "react";
  const Cards = () => {
     return ( 
-    <div className="card" style="width: 18rem;">
+      <div className="card" style={{ width: '18rem' }}>
   <img className="card-img-top" src="..." alt="Card image cap"></img>
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
